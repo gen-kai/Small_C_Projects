f47892e66cb419758976c32bf5d5d82898dd7a88
gen-kai <87318319+gen-kai@users.noreply.github.com>
2025-07-27 14:00:27 -0700

    NtCreateFile_Direct_Syscall initial commit

---
+++ NtCreateFile_Direct_Syscall/syscalls.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <windows.h>
+#include <winternl.h>
+
+extern NTSTATUS __stdcall SyscallNtCreateFile(
+	PHANDLE fileHandle,
+	ACCESS_MASK desiredAccess,
+	POBJECT_ATTRIBUTES objectAttributes,
+	PIO_STATUS_BLOCK processIoStatusBlock,
+	PLARGE_INTEGER allocationSize,
+	ULONG fileAttributes,
+	ULONG shareAccess,
+	ULONG createDisposition,
+	ULONG createOptions,
+	PVOID eaBuffer,
+	ULONG eaLength);
